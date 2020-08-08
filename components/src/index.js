ebd094f65f21d8355c46267b35e276519ed90130
Hunva <ttanhvu1712@gmail.com>
2020-08-08 19:02:54 +0700

    3.16 Component reusable

--- components/src/index.js
+++ components/src/index.js
@@ -1,31 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './components/CommentDetail'
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./components/CommentDetail";
+import ApprovalCard from "./components/ApprovalCard";
 
 const App = () => {
 	return (
-		<div className="ui container comments"> 
-			<CommentDetail 
-				author={faker.name.findName()} 
-				timeAgo={"Today at 4:45PM"} 
-				content={"Comment 01"}
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail 
-				author={faker.name.findName()} 
-				timeAgo={"Today at 5:45PM"} 
-				content={"Comment 02"}
-				avatar={faker.image.avatar()}
+		<div className="ui container comments">
+			<ApprovalCard>
+				<CommentDetail
+					author={faker.name.findName()}
+					timeAgo={"Today at 4:45PM"}
+					content={"Comment 01"}
+					avatar={faker.image.avatar()}
 				/>
-			<CommentDetail 
-				author={faker.name.findName()} 
-				timeAgo={"Today at 6:45PM"} 
-				content={"Comment 03"}
-				avatar={faker.image.avatar()}
-			/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author={faker.name.findName()}
+					timeAgo={"Today at 5:45PM"}
+					content={"Comment 02"}
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author={faker.name.findName()}
+					timeAgo={"Today at 6:45PM"}
+					content={"Comment 03"}
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
-	)
+	);
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
