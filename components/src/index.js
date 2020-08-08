dc11883a9b434f910614bd7e81303d2344f92665
Hunva <ttanhvu1712@gmail.com>
2020-08-08 17:21:37 +0700

    3.8 Component nesting

--- components/src/index.js
+++ components/src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './components/CommentDetail'
 
 const App = () => {
     return (
         <div className="ui container comments"> 
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Ranhatti Hunva
-                    </a>
-                    <div className="metadata">
-                        <span className="date"> Today at 60:00 PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
         </div>
     )
 };
