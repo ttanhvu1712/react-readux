e7b53be3f3d728ca7c3e81ebb8a26b1ef5904b0f
Hunva <ttanhvu1712@gmail.com>
2020-08-08 18:22:13 +0700

    3.10 Props

--- components/src/components/CommentDetail.js
+++ components/src/components/CommentDetail.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
     return (
         <div className="ui container comments"> 
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
+                    <img alt="avatar" src={props.avatar}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                        Ranhatti Hunva
+                        {props.author}
                     </a>
                     <div className="metadata">
-                        <span className="date"> Today at 60:00 PM</span>
+                        <span className="date"> {props.timeAgo} </span>
                     </div>
-                    <div className="text">Nice blog post!</div>
+                    <div className="text"> {props.content} </div>
                 </div>
             </div>
         </div>
--- components/src/index.js
+++ components/src/index.js
@@ -4,13 +4,28 @@ import faker from 'faker';
 import CommentDetail from './components/CommentDetail'
 
 const App = () => {
-    return (
-        <div className="ui container comments"> 
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-        </div>
-    )
+	return (
+		<div className="ui container comments"> 
+			<CommentDetail 
+				author={faker.name.findName()} 
+				timeAgo={"Today at 4:45PM"} 
+				content={"Comment 01"}
+				avatar={faker.image.avatar()}
+			/>
+			<CommentDetail 
+				author={faker.name.findName()} 
+				timeAgo={"Today at 5:45PM"} 
+				content={"Comment 02"}
+				avatar={faker.image.avatar()}
+				/>
+			<CommentDetail 
+				author={faker.name.findName()} 
+				timeAgo={"Today at 6:45PM"} 
+				content={"Comment 03"}
+				avatar={faker.image.avatar()}
+			/>
+		</div>
+	)
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'))
