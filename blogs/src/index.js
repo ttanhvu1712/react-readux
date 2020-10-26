882ae1e32c71e5f9eda4d22b5979992606295a14
anhvu.tran <anhvu.tran@shopee.com>
2020-10-26 21:41:45 +0700

    * Add yarn.lock

--- blogs/src/index.js
+++ blogs/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}>
     <App />
   </Provider>,
   document.querySelector('#root')
