7949f85e67a720dd267f6661a17b578759646eae
anhvu.tran <anhvu.tran@shopee.com>
2020-10-30 10:47:12 +0700

    22.08 Add rtmp server

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -38,7 +38,7 @@ class StreamList extends Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/show/${stream.id}`}>{stream.title}</Link>
             <div className="description"> {stream.description} </div>
           </div>
         </div>
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-export default function StreamShow() {
-  return (
-    <div>
-      StreamShow
-    </div>
-  )
+class StreamShow extends Component {
+  componentDidMount() {
+    const { fetchStream, match } = this.props;
+    fetchStream(match.params.id);
+  }
+
+  render() {
+    const { stream } = this.props
+    if(stream) {
+      return (
+        <div>
+          <h1>{stream.title}</h1>
+          <h5>{stream.description}</h5>
+        </div>
+      )
+    }
+    return <div>Loading...</div>;
+  }
 }
+
+export default connect(
+  ({ streams }, { match }) => ({ stream: streams[match.params.id] }),
+  {fetchStream}
+)(StreamShow);
