80f88877688851164a7667f1ac99f1c5eacf0b28
anhvu.tran <anhvu.tran@shopee.com>
2020-10-30 11:33:59 +0700

    22.13. Finished stream project

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,22 +1,50 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from "flv.js";
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     const { fetchStream, match } = this.props;
     fetchStream(match.params.id);
+
+    this.buildPlayer()
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer()
+  }
+
+  componentWillUnmount() {
+    this.flvPlayer.destroy()
+  }
+
+  buildPlayer() {
+    if (this.flvPlayer || !this.props.stream) return;
+
+    this.flvPlayer = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
   }
 
   render() {
-    const { stream } = this.props
-    if(stream) {
+    const { stream } = this.props;
+    if (stream) {
       return (
         <div>
+          <video ref={this.videoRef} style={{ width: "100%" }} controls></video>
           <h1>{stream.title}</h1>
           <h5>{stream.description}</h5>
         </div>
-      )
+      );
     }
     return <div>Loading...</div>;
   }
@@ -24,5 +52,5 @@ class StreamShow extends Component {
 
 export default connect(
   ({ streams }, { match }) => ({ stream: streams[match.params.id] }),
-  {fetchStream}
+  { fetchStream }
 )(StreamShow);
